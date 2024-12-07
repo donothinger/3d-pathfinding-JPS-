f58dd77213b9cf7a16e78fb88e14988d36d4c744
donothinger <sier.pr@mail.ru>
2024-12-07 22:03:17 +0300

    JPS realisation

--- Map.py
+++ Map.py
@@ -13,7 +13,7 @@ class Map:
         self._cells = cells
 
     def in_bounds(self, coord: Tuple[int, int, int]) -> bool:
-        return 0 <= coord[0] <= self._height and 0 <= coord[1] <= self._width and 0 <= coord[2] <= self._depth
+        return 0 <= coord[0] < self._height and 0 <= coord[1] < self._width and 0 <= coord[2] < self._depth
 
     def traversable(self, coord: Tuple[int, int, int]) -> bool:
         return not self._cells[coord]
@@ -197,54 +197,19 @@ class Map:
             neighbors.append((x - 1, y - 1, z - 1))
 
         return neighbors
-        
-    def jump(self, coord, direction, goal, recursive=False):
-        i, j, k = coord
-        dx, dy, dz = direction
-        new_coord = i + dx, j + dy, k + dz
-        if not self.walkable(new_coord):
-            return None
-        if new_coord == goal:
-            return new_coord
-        for neigh in self.get_neighbors(new_coord):
-            if neigh[3]:
-                return new_coord
-        if not recursive:
-            if 0 not in direction: # 3d-diagonal
-                canonical_directions = [(dx, 0, 0), (0, dy, 0), (0, 0, dz), (dx, dy, 0), (dx, 0, dz), (0, dy, dz)]
-                for can_direction in canonical_directions:
-                    if self.jump(new_coord, can_direction, goal, recursive=True) is not None:
-                        return new_coord
-            if sum(direction) not in (-1, 1): # 2d-diagonal
-                canonical_directions = [(dx, 0, 0), (0, dy, 0), (0, 0, dz)]
-                canonical_directions.remove((0, 0, 0))
-                for can_direction in canonical_directions:
-                    if self.jump(new_coord, can_direction, goal, recursive=True) is not None:
-                        return new_coord
-        return self.jump(new_coord, direction, goal)
 
-    def get_successors(self, coord: Tuple[int, int, int], goal: Tuple[int, int, int]) -> List[Tuple[int, int, int]]:
-        successors = []
-        coord_steps = [-1, 0, 1]
-        delta = [(x, y, z) for x in coord_steps for y in coord_steps for z in coord_steps]
-        delta.remove((0, 0, 0))
-        delta = tuple(delta)
-        for direction in delta:
-            successors.append(self.jump(coord, direction, goal))
-        successors = filter(None, successors)
-        return successors
-
+    def get_successors(self, coord: Tuple[int, int, int], 
+                             goal:  Tuple[int, int, int] = (0, 0, 0)) -> List[Tuple[int, int, int]]:
+        return self.get_neighbors(coord)
+    
     def get_size(self) -> Tuple[int, int, int]:
         return self._height, self._width, self._depth
     
-def compute_cost(coord1: Tuple[int, int, int],
-                 coord2: Tuple[int, int, int]) -> Union[int, float]:
-    i1, j1, k1 = coord1
-    i2, j2, k2 = coord2
-    if abs(i1 - i2) <= 1 and abs(j1 - j2) <= 1 and abs(k1 - k2) <= 1:
+    def compute_cost(self, coord1: Tuple[int, int, int],
+                     coord2: Tuple[int, int, int]) -> Union[int, float]:
+        i1, j1, k1 = coord1
+        i2, j2, k2 = coord2
         return sqrt(abs(i1 - i2) + abs(j1 - j2) + abs(k1 - k2))
-    else:
-        raise ValueError("Trying to compute the cost of a non-supported move! ONLY cardinal moves are supported.")
     
 def read_cells_from_file(file_path: str) -> npt.NDArray:
     file = open(file_path)
--- astar.py
+++ astar.py
@@ -1,7 +1,11 @@
-from typing import Callable, Type, Tuple, Optional, Iterable
+from typing import Callable, Type, Tuple, Optional, Iterable, Union
 from Node import Node
 from SearchTreePQD import SearchTreePQD
-from Map import Map, compute_cost
+from Map import Map
+
+def zero_heuristic(start_coord: Tuple[int, int, int],
+                    goal_coord: Tuple[int, int, int]) -> Union[int, float]:
+    return 0
 
 def astar(
     task_map: Map,
@@ -10,24 +14,42 @@ def astar(
     heuristic_func: Callable,
     search_tree: Type[SearchTreePQD],
 ) -> Tuple[bool, Optional[Node], int, int, Optional[Iterable[Node]], Optional[Iterable[Node]]]:
+    
+    start_node = Node(start_coord, g=0, 
+                      h=heuristic_func(start_coord, goal_coord))
+    goal_node = Node(goal_coord, g=0,
+                     h=0)
+    return astar_from_nodes(task_map=task_map,
+                 start_node=start_node,
+                 goal_node=goal_node,
+                 heuristic_func=heuristic_func,
+                 search_tree=search_tree)
+
+def astar_from_nodes(
+    task_map: Map,
+    start_node: Node,
+    goal_node:  Node,
+    heuristic_func: Callable,
+    search_tree: Type[SearchTreePQD],
+) -> Tuple[bool, Optional[Node], int, int, Optional[Iterable[Node]], Optional[Iterable[Node]]]:
 
     ast = search_tree()
     steps = 0
 
-    start_node = Node(start_coord , g=0,\
-                      h=heuristic_func(start_coord, goal_coord))
     ast.add_to_open(start_node)
 
     while(not ast.open_is_empty()):
         steps += 1
         current_node = ast.get_best_node_from_open()
         ast.add_to_closed(current_node)
-        if(current_node.coord == goal_coord):
+
+        if(current_node.coord == goal_node.coord):
             print("During the search, the following number of OPEN dublicates was encountered: ", ast.number_of_open_duplicates)
             return True, current_node, steps, len(ast), ast.opened, ast.expanded
-        for coord in task_map.get_successors(current_node.coord):
-            s_new = Node(coord=coord, g=current_node.g + compute_cost(current_node.coord, coord),\
-                          h=heuristic_func(coord, goal_coord), parent=current_node)
+
+        for coord in task_map.get_successors(current_node.coord, goal_node.coord):
+            s_new = Node(coord=coord, g=current_node.g + task_map.compute_cost(current_node.coord, coord),\
+                          h=heuristic_func(coord, goal_node.coord), parent=current_node)
             if not ast.was_opened(s_new):
                 ast.add_to_open(s_new)
             else:
