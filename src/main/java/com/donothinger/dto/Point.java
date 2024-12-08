25e507ed2e0ad51326a89d550be89bc2a17e23ad
donothinger <sier.pr@mail.ru>
2024-12-08 14:54:04 +0300

    Map realisation

---
+++ src/main/java/com/donothinger/dto/Point.java
@@ -0,0 +1,12 @@
+package com.donothinger.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Point {
+    public Integer x;
+    public Integer y;
+    public Integer z;   
+}
