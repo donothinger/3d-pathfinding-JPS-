3988f96dcc54063ce4bdf7feca02c3e5e5eefb09
donothinger <sier.pr@mail.ru>
2024-12-08 15:23:37 +0300

    Node realisation

---
+++ src/main/java/com/donothinger/dto/Node.java
@@ -0,0 +1,23 @@
+package com.donothinger.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "point")
+public class Node {
+    public Point point;
+    public Double g;
+    public Double h;
+    public Double f;
+    public Node parent;
+
+    public Node(Point point, Double g, Double h) {
+        this.point = point;
+        this.g = g;
+        this.h = h;
+        this.f = g + h;
+    }
+}
--- src/main/java/com/donothinger/dto/Point.java
+++ src/main/java/com/donothinger/dto/Point.java
@@ -2,9 +2,11 @@ package com.donothinger.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
 @Data
+@EqualsAndHashCode(exclude = {"x", "y", "z"})
 public class Point {
     public Integer x;
     public Integer y;
